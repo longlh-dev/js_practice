0ce80ec4e363fcf04bd63afdf0c254c7b4249845
longlh-dev <hoanglongly942@gmail.com>
2024-03-27 17:33:06 +0700

    updated

--- note-app/src/script.js
+++ note-app/src/script.js
@@ -40,3 +40,23 @@ document.addEventListener("keydown", event => {
   }
 })
 showNotes();
+
+
+
+//
+
+
+// console.log("hello")
+
+// function doHomework(subject, callback) {
+//   setTimeout( function(){
+//       console.log(`Bắt đầu làm bài tập ${subject}.`);
+//       callback();
+//   }, 5000);
+  
+// }
+
+// function alertFinished(){
+//   console.log('Làm bài tập xong!');
+// }
+// doHomework('Toán', alertFinished);
